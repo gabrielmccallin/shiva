00288aa9c4799e45ecf0103f862764995146dfd2
Hao Dong <dong_hao@hotmail.co.uk>
2017-11-07 16:00:49 +0000

    Merged in hao-dong/shiva/feature/add-options-to-select (pull request #48)
    Added options Getter and Setter to Select

--- test/Select.test.ts
+++ test/Select.test.ts
@@ -2,7 +2,6 @@ import { Select } from '../src/Select';
 
 describe("SELECT", function () {
   describe("constructor", function () {
-
     test("constructed", function () {
       const container = new Select({
         options: [
@@ -11,10 +10,12 @@ describe("SELECT", function () {
         ]
       });
 
-      const option = container.element.children.item(0);
-      expect(option.innerHTML).toEqual("shiva");
+      const option = container.element.firstChild;
+      expect(option.textContent).toEqual("shiva");
     });
+  });
 
+  describe('Selected Index', () => {
     test("get selected index", function () {
       const container = new Select({
         options: [
@@ -37,6 +38,36 @@ describe("SELECT", function () {
 
       expect(container.selectedIndex).toEqual(1);
     });
+  });
 
+  describe('Options', () => {
+    test("get select options", function () {
+      const options = [
+        { value: "hello", text: "shiva" },
+        { value: "goodbye", text: "shiva" }
+      ];
+      const container = new Select({
+        options
+      });
+
+      expect(container.options).toEqual(options);
+    });
+
+    test("set select options", function () {
+      const options = [
+        { value: "hello", text: "shiva" },
+        { value: "goodbye", text: "shiva" }
+      ];
+      const container = new Select({
+        options: []
+      });
+      
+      expect(container.element.children.length).toEqual(0);
+
+      container.options = options;
+
+      expect(container.element.children.length).toEqual(2);
+      expect(container.element.firstChild.textContent).toBe('shiva');
+    });
   });
 });
